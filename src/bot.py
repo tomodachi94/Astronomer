98e8b74f51fe58d42b9fe7ffb8643f6078972518
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-07 20:48:38 -0800

    Add imports() and setallenv() funcs to src/bot.py
    Also added some comments.

--- src/bot.py
+++ src/bot.py
@@ -2,3 +2,5 @@
 
 import os #The one import outside of src/envir.py
 os.system("python src/envir.py") #Runs src/envir.py to set functions
+imports() #Imports deps
+setallenv() #Creates variable for token and prefix
