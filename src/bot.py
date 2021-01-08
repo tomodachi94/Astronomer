16a066c3f48a6d72ee3e10228125655830d87449
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-08 07:34:41 +0000

    Make envir into an API, implement API

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,5 @@
 # src/bot.py
-
-import os #The one import outside of src/envir.py
-os.system("python src/envir.py") #Runs src/envir.py to set functions
-imports() #Imports deps
-setallenv() #Creates variable for token and prefix
+import envir
+#functions
+envir.imports() #Imports deps
+envir.setallenv() #Creates variable for token and prefix
--- src/envir.py
+++ src/envir.py
@@ -1,7 +1,12 @@
 def imports():
+  import os
   from dotenv import load_dotenv
 
 def setallenv():
+  import os
   from dotenv import load_dotenv
   DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
   DISCORD_BOT_PREFIX = os.getenv("DISCORD_BOT_PREFIX")
+
+def update():
+  os.system("git pull")
