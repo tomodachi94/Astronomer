f5227a7b10b7fd7c27c12416a9a27342358e89c0
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-08 08:03:56 +0000

    Rename envir to helper

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,5 @@
 # src/bot.py
-import envir
-#functions
-envir.imports() #Imports deps
-envir.setallenv() #Creates variable for token and prefix
+import helper
+#Start loading stuff
+helper.imports() #Imports deps
+helper.setallenv() #Creates variable for token and prefix
