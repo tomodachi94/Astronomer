522df26aafd1cd218617dc17f49ce68d4057b5fe
Tomodachi94 <tomodachi94@protonmail.com>
2021-01-14 08:32:58 +0000

    Prep for using aiosqlite

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ import os
 import helper
 import discord
 from discord.ext import commands
+import aiosqlite
 #Start loading stuff
 helper.imports() #Imports deps
 helper.setallenv() #Creates variable for token and prefix
@@ -15,6 +16,6 @@ async def on_ready():
 	print("[Astronomer/src/bot.py:Status] Setting status")
 	await bot.change_presence(
 	    activity=discord.Game(os.getenv("DISCORD_BOT_PREFIX") + "help for help"))
-	print(__main__ + ":Status] Status set")
+	print(":Status] Status set")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
